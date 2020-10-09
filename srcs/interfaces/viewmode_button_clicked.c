64c7dfa64bafc753806a89d41652cfd298cbc333
hcabel <hcabel@student.42.fr>
2020-10-09 10:30:09 +0200

    don't recalculate viewport if you change viewmode with the current used

--- srcs/interfaces/viewmode_button_clicked.c
+++ srcs/interfaces/viewmode_button_clicked.c
@@ -6,7 +6,7 @@
 /*   By: hcabel <hcabel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 18:26:50 by hcabel            #+#    #+#             */
-/*   Updated: 2020/10/08 12:10:01 by hcabel           ###   ########.fr       */
+/*   Updated: 2020/10/09 10:28:50 by hcabel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,24 @@
 
 void	set_lit_viewmode(t_info *info)
 {
-	info->scene.cam.viewmode = 0;
+	if (info->scene.cam.viewmode != 0)
+		info->scene.cam.viewmode = 0;
 }
 
 void	set_unlit_viewmode(t_info *info)
 {
-	info->scene.cam.viewmode = 1;
+	if (info->scene.cam.viewmode != 1)
+		info->scene.cam.viewmode = 1;
 }
 
 void	set_normalmap_viewmode(t_info *info)
 {
-	info->scene.cam.viewmode = 2;
+	if (info->scene.cam.viewmode != 2)
+		info->scene.cam.viewmode = 2;
 }
 
 void	set_iteration_viewmode(t_info *info)
 {
-	info->scene.cam.viewmode = 3;
+	if (info->scene.cam.viewmode != 3)
+		info->scene.cam.viewmode = 3;
 }
